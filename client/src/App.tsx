4fcf7c0d6535fd4cd7e580cbbbce87d35e819f40
sksayril <sksayril123@gmail.com>
2026-01-25 16:33:58 +0530

    fixed the issues

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,6 +3,7 @@ import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
+import { AgeVerification } from "@/components/AgeVerification";
 import NotFound from "@/pages/not-found";
 import Home from "@/pages/Home";
 import VideoDetail from "@/pages/VideoDetail";
@@ -24,6 +25,7 @@ function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <TooltipProvider>
+        <AgeVerification />
         <Toaster />
         <Router />
       </TooltipProvider>
